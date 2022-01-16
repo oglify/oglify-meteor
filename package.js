04e6bf32832b8295784e9a1c3fd5ec8453ca42ff
brendansafr <brendansafr@gmail.com>
2022-01-16 16:26:16 -0500

    Draft 1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "oglify:oglify",
-  version: "0.0.1",
+  version: "0.0.2",
   summary: "The core meteor package for OGLify",
   git: "https://github.com/oglify/oglify-meteor",
   documentation: "README.md",
@@ -17,12 +17,12 @@ Npm.depends({
 
 Package.onUse(function (api) {
   api.versionsFrom("2.5.3");
-  api.use(["ecmascript", "oglify:react@0.0.1", "meteor"]);
+  api.use(["ecmascript", "oglify:react@0.0.2", "meteor"]);
   api.mainModule("oglify.js");
 });
 
 Package.onTest(function (api) {
-  api.use(["ecmascript", "oglify:react@0.0.1", "meteor"]);
+  api.use(["ecmascript", "oglify:react@0.0.2", "meteor"]);
   api.use("tinytest");
   api.use("oglify:oglify");
   api.mainModule("oglify-tests.js");
